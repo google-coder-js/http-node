3c9dc75a1b4419cb4787dea610a73a8be60f1c91
Rohit Bhardwaj <95678813+google-coder-js@users.noreply.github.com>
2022-10-01 00:03:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const https = require("http");
+const http = require("http");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const httpServer = https.createServer(
+const httpServer = http.createServer(
 //   {
 //     key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
 //     cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
