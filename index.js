b6d542be08a2f47ec548a5562a6080a6ddd12c34
Rohit Bhardwaj <95678813+google-coder-js@users.noreply.github.com>
2022-10-01 00:02:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const https = require("https");
+const https = require("http");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
@@ -14,14 +14,14 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const httpsServer = https.createServer(
-  {
-    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
-  },
+const httpServer = https.createServer(
+//   {
+//     key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+//     cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+//   },
   app
 );
 
-httpsServer.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
